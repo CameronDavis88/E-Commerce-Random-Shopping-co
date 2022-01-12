9d5fd3b45e240ca602f3f4201712478d054c2df5
CameronDavis88 <cammien88@gmail.com>
2022-01-12 12:47:05 -0700

    Shipping subdivisions are coming through but something is not working with the shipping options.

--- src/components/Checkout/AddressFrom.js
+++ src/components/Checkout/AddressFrom.js
@@ -15,21 +15,48 @@ const AddressFrom = ({ checkoutToken }) => {
     const methods = useForm();
     //shippingCountries comes back as an object this turns it into and array of objects each with an id and label key which is the name of country
     const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name }))
-    console.log(countries)
+    const subdivisions = Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name }))
+    //shippingOptions is already an array
+    const options = shippingOptions.map((shippingObj) => ({ id: shippingObj.id, label: `${shippingObj.description} - (${shippingObj.price.formatted_with_symbol})`}))
+
+    console.log(subdivisions)
+    console.log(shippingOptions)
+
 
     const fetchShippingCountries = async function (checkoutTokenId) {
         const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
         console.log(countries);
         setShippingCountries(countries);
         setShippingCountry(Object.keys(countries[0]));
+    }
 
+    const fetchSubdivisions = async function(countryCode){
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+        setShippingSubdivisions(subdivisions);
+        setShippingSubdivision(Object.keys(subdivisions[0]));
+    }
+
+    const fetchShippingOptions = async function(checkoutTokenId, country, region = null){
+        const { options } = await commerce.getShippingOptions(checkoutTokenId, {country, region});
+        setShippingOptions(options);
+        console.log(options);
+        setShippingOption(options[0].id);
     }
 
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id)
     }, []);
 
+    useEffect(() => {
+        if(shippingCountry) fetchSubdivisions(shippingCountry)
+    }, [shippingCountry]);
 
+    useEffect(() => {
+        console.log(options)
+        if(shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
+    }, [shippingSubdivision]);
+
+    
 
     return (
         //Employing React hook form for all the inputs
@@ -39,44 +66,39 @@ const AddressFrom = ({ checkoutToken }) => {
                 <form onSubmit='' >
                     <Grid container spacing={3} >
                         {/* CustomInput is a reusable input component I created using react-hook-form */}
-                        <CustomInput required name='first name' label='First name' />
-                        <CustomInput required name='last name' label='Last name' />
-                        <CustomInput required name='address1' label='Address' />
-                        <CustomInput required name='email' label='Email' />
-                        <CustomInput required name='city' label='City' />
-                        <CustomInput required name='zip' label='ZIP code' />
-
+                        <CustomInput name='first name' label='First name' />
+                        <CustomInput name='last name' label='Last name' />
+                        <CustomInput name='address1' label='Address' />
+                        <CustomInput name='email' label='Email' />
+                        <CustomInput name='city' label='City' />
+                        <CustomInput name='zip' label='ZIP code' />
                         <Grid type item xs={12} sm={6} >
                             <InputLabel>Shipping Country</InputLabel>
                             <Select value={shippingCountry} fillWidth onChange={(e) => setShippingCountry(e.target.value)} >
                                 {countries.map((country) => (
-                                    <MenuItem key={country.id} value={country.id}>
-                                        {country.label}
-                                    </MenuItem>
+                                    <MenuItem key={country.id} value={country.id}>{country.label}</MenuItem>
                                 ))}
 
                             </Select>
                         </Grid>
-
-                        {/* <Grid type item xs={12} sm={6} >
+                        <Grid type item xs={12} sm={6} >
                             <InputLabel>Shipping Subdivision</InputLabel>
-                            <Select value={} fillWidth onChange={} >
-                                <MenuItem key={} value={}>
-                                    Select me
-                                </MenuItem>
+                            <Select value={shippingSubdivision} fillWidth onChange={(e) => setShippingSubdivision(e.target.value)} >
+                            {subdivisions.map((subdivision) => (
+                                    <MenuItem key={subdivision.id} value={subdivision.id}>{subdivision.label} </MenuItem>
+                                ))}
+                                
                             </Select>
                         </Grid>
-
                         <Grid type item xs={12} sm={6} >
                             <InputLabel>Shipping Options</InputLabel>
-                            <Select value={} fillWidth onChange={} >
-                                <MenuItem key={} value={}>
-                                    Select me
-                                </MenuItem>
+                            <Select value={shippingOption} fillWidth onChange={(e) => setShippingOption(e.target.value)} >
+                            {options.map((option) => (
+                                    <MenuItem key={option.id} value={option.id}>{option.label} </MenuItem>
+                                ))}
+                                
                             </Select>
-                        </Grid> */}
-
-
+                        </Grid>
                     </Grid>
                 </form>
             </FormProvider>
