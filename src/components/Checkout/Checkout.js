4572385faf0953a3270f313b0a3c26fd0d9c156f
CameronDavis88 <cammien88@gmail.com>
2022-01-18 12:01:30 -0700

    Did styling of Checkout component.

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,33 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import { commerce } from '../../library/commerce';
+import useStyles from './styles';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = ({ cart, error, onCaptureCheckout, order, refreshCart }) => {
+    const classes = useStyles();
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState('');
     const [shippingData, setShippingData] = useState({});
 
     useEffect(() => {
         console.log(order)
-    }, [])
+    }, []);
 
     useEffect(
         () => {
-        const generateToken = async function(){
-            try{
-                const token = await commerce.checkout.generateToken(cart.id, { type:'cart' });
-                setCheckoutToken(token);
-            } catch(error) {
-                console.log(error)
+            const generateToken = async function () {
+                try {
+                    const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+                    setCheckoutToken(token);
+                } catch (error) {
+                    console.log(error)
+                }
             }
-        }
-        generateToken();
-    }, [cart]);
+            generateToken();
+        }, [cart]);
 
     const nextStep = () => {
         setActiveStep((previousActiveStep) => previousActiveStep + 1);
@@ -40,47 +42,49 @@ const Checkout = ({ cart, error, onCaptureCheckout, order, refreshCart }) => {
         nextStep();
     }
 
-    let ConfirmationForm = () => 
-    //  order.customer ? (
-            <>
+    let ConfirmationForm = () =>
+        //  order.customer ? (
+        <>
             <div>
                 <Typography variant='h5' >Thank you for your purchase, {shippingData.firstName} {shippingData.lastName}!</Typography>
-                <Divider />
+                <Divider className={classes.divider} />
                 <Typography variant='subtitle2' >
                     *Don't worry, transaction did not actually occur.
                 </Typography>
             </div>
-            <br/>
+            <br />
             <Button component={Link} to="/" variant='outlined' type='button'>Return to Home Page</Button>
-            </>
-            //This conditional rendering was commented out to allow confirmation page to be rendered without actual order going through
+        </>
+    //This conditional rendering was commented out to allow confirmation page to be rendered without actual order going through
     // ) : (
-    //     <div>
+    //     <div classes.spinner >
     //         <CircularProgress  />
     //     </div>
     // );
 
     // if(error){
-        // <>
-        // <Typography variant='h5' >Error: {error}</Typography>
-        // <br/>
-        // <Button component={Link} to="/" variant='outlined' type='button'></Button>
-        // </>
-    
+    // <>
+    // <Typography variant='h5' >Error: {error}</Typography>
+    // <br/>
+    // <Button component={Link} to="/" variant='outlined' type='button'></Button>
+    // </>
+
 
     //conditionally rendering the form
     const Form = () => (
         activeStep === 0
-            ? <AddressForm checkoutToken={checkoutToken} next={next} nextStep={nextStep}/>
+            ? <AddressForm checkoutToken={checkoutToken} next={next} nextStep={nextStep} />
             : <PaymentForm refreshCart={refreshCart} error={error} order={order} nextStep={nextStep} checkoutToken={checkoutToken} shippingData={shippingData} backStep={backStep} onCaptureCheckout={onCaptureCheckout} />
     )
 
     return (
         <>
-            <div className='spaceMaker' />
-            <main>
+            <CssBaseline />
+            <div className={classes.toolbar} />
+            <main className={classes.layout} >
+                <Paper className={classes.paper} />
                 <Typography variant='h4' align='center' >Checkout</Typography>
-                <Stepper activeStep={0} >
+                <Stepper activeStep={0} className={classes.stepper} >
                     {steps.map(step => (
                         <Step key={step} >
                             <StepLabel>{step}</StepLabel>
@@ -89,8 +93,8 @@ const Checkout = ({ cart, error, onCaptureCheckout, order, refreshCart }) => {
                     )}
                 </Stepper>
 
-    {/* If it is in the last step display the Confirmation Form for review if not display From if token was returned*/}
-                {activeStep === steps.length ? <ConfirmationForm  /> : checkoutToken && <Form /> }
+                {/* If it is in the last step display the Confirmation Form for review if not display From if token was returned*/}
+                {activeStep === steps.length ? <ConfirmationForm /> : checkoutToken && <Form />}
             </main>
         </>
     )
